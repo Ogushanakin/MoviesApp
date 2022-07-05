6dad3c00a483a2402ff0bb3daa8197327639a2e8
Ogushanakin <ogzhnakn96@gmail.com>
2022-07-05 11:26:17 +0300

    UpcomingPage

---
+++ Badflix/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Badflix
+//
+//  Created by AKIN on 5.07.2022.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
